8036392ff3269889d728ef5d2b1ff64257d34948
Ayoazeez26 <44169294+Ayoazeez26@users.noreply.github.com>
2026-03-25 22:52:31 +0100

    feat: completed 01 - 09 challenges

--- submission/03.sh
+++ submission/03.sh
@@ -1,3 +1,9 @@
 # Create a SegWit address.
 # Add funds to the address.
 # Return only the Address
+
+NEW_ADDR=$(bitcoin-cli -regtest getnewaddress "" "bech32")
+bitcoin-cli -regtest generatetoaddress 101 "$NEW_ADDR" > /dev/null
+bitcoin-cli -regtest sendtoaddress "$NEW_ADDR" 0.01
+
+echo $NEW_ADDR
